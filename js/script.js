0726c3e821b444fc419edfeede797a6a76127219
Junaid Sarguroh <junaidsar17@gmail.com>
2023-05-21 02:19:42 +0100

    About and Portfolio

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+//   $(document).ready(function(){
+//       $("#01").mouseover(function(){
+//           $("#01").show();
+//       });
+//       $("#01").mouseout(function(){
+//           $("#01").hide();
+//       });
+//   });
+
+// function bigImg(x) {
+//    x.style.height = "1000px";
+//    x.style.width = "1000px";
+// }
+
+// function normalImg(x) {
+//    x.style.height = "150px";
+//    x.style.width = "150px";
+// }
+
+
+/*<img onmouseover="bigImg(this)" onmouseout="normalImg(this)" border="0" src="smiley.gif" alt="Smiley" width="32" height="32"></img>*/
+  
